88978f0a4d7332543699eff09ea17e7a1778fecb
rwoodill <woodillrachel@gmail.com>
2024-04-05 15:22:33 -0400

    forecast widget for next 7 days

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, OPEN_WEATHER_API_KEY } from "./api";
 import { useState } from "react";
 
@@ -32,13 +33,11 @@ function App() {
       .catch((err) => console.log(err));
   };
 
-  console.log(currentWeather);
-  console.log(forecast);
-
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
